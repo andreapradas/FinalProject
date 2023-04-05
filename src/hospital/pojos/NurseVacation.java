a25e6265b3d24a48115b3478383583f4a46c2b57
Paula <Paula@DESKTOP-3O3169P>
2023-04-05 14:21:30 +0200

    NurseVacation+ww

--- src/hospital/pojos/NurseVacation.java
+++ src/hospital/pojos/NurseVacation.java
@@ -1,5 +1,79 @@
 package hospital.pojos;
 
-public class NurseVacation {
+import java.time.LocalDate;
+import java.util.Objects;
 
+public class NurseVacation {
+	private Integer vacationID;
+	private Integer nurseID;
+	private LocalDate starts;
+	private LocalDate ends;
+	
+	
+	public NurseVacation() {
+		super();
+	}
+	
+	public NurseVacation(Integer vacationID, Integer nurseID, LocalDate starts, LocalDate ends) {
+		super();
+		this.vacationID = vacationID;
+		this.nurseID = nurseID;
+		this.starts = starts;
+		this.ends = ends;	
+	}
+	
+	@Override
+	public String toString() {
+		return 
+				"NurseVacation [VacationID=" + vacationID + ", nurseID=" + nurseID + ", starts=" + starts + ", ends=" + ends + "]";
+	}
+
+	public Integer getVacationID() {
+		return vacationID;
+	}
+
+	public void setVacationID(Integer vacationID) {
+		this.vacationID = vacationID;
+	}
+	
+	public Integer getNurseID() {
+		return nurseID;
+	}
+
+	public void setNurseId(Integer nurseID) {
+		this.nurseID = nurseID;
+	}
+	
+	public LocalDate getStarts() {
+		return starts;
+	}
+
+	public void setStarts(LocalDate starts) {
+		this.starts = starts;
+	}
+	
+	public LocalDate getEnds() {
+		return ends;
+	}
+
+	public void setEnds(LocalDate ends) {
+		this.ends = ends;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(vacationID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NurseVacation other = (NurseVacation) obj;
+		return Objects.equals(vacationID, other.vacationID);
+	}
 }
---
+++ src/hospital/pojos/WorksWith.java
@@ -0,0 +1,79 @@
+package hospital.pojos;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class WorksWith {
+	private Integer teamID;
+	private Integer surgeonID;
+	private Integer nurseID;
+	private LocalDate date;
+	
+	
+	public WorksWith() {
+		super();
+	}
+	
+	public WorksWith(Integer teamID, Integer surgeonID, Integer nurseID, LocalDate date) {
+		super();
+		this.teamID = teamID;
+		this.surgeonID = surgeonID;
+		this.nurseID = nurseID;
+		this.date = date;	
+	}
+	
+	@Override
+	public String toString() {
+		return 
+				"WorksWith [teamID=" + teamID + ", surgeonID=" + surgeonID + ", nurseID=" + nurseID + ", date=" + date + "]";
+	}
+
+	public Integer getTeamID() {
+		return teamID;
+	}
+
+	public void setTeamID(Integer teamID) {
+		this.teamID = teamID;
+	}
+	
+	public Integer getSurgeonID() {
+		return surgeonID;
+	}
+
+	public void setSurgeonID(Integer surgeonID) {
+		this.surgeonID = surgeonID;
+	}
+	
+	public Integer getNurseID() {
+		return nurseID;
+	}
+
+	public void setNurseID(Integer nurseID) {
+		this.nurseID = nurseID;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(teamID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WorksWith other = (WorksWith) obj;
+		return Objects.equals(teamID, other.teamID);
+	}
+}
