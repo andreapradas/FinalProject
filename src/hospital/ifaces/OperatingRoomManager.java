92007d901f080b064c5bdb49624061d2a63b98df
andreapradas <andrea.pradasagujetas@usp.ceu.es>
2023-04-14 10:52:53 +0200

    gfhj

--- src/hospital/ifaces/OperatingRoomManager.java
+++ src/hospital/ifaces/OperatingRoomManager.java
@@ -1,5 +1,5 @@
 package hospital.ifaces;
 
-public class OperatingRoomManager {
+public interface OperatingRoomManager {
 
 }
--- src/hospital/ifaces/PatientManager.java
+++
@@ -1,5 +0,0 @@
-package hospital.ifaces;
-
-public class PatientManager {
-
-}
